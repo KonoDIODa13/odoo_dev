1a2ee3d139d497188d1f7c04061fab98df50fe76
jaime.gonbra <jaime.gonbra@educa.jcyl.es>
2024-11-14 13:12:16 +0100

    controlador creado

--- addons/filmotecajaime/controllers/controllers.py
+++ addons/filmotecajaime/controllers/controllers.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # from odoo import http
 
-
 # class Filmotecajaime(http.Controller):
 #     @http.route('/filmotecajaime/filmotecajaime', auth='public')
 #     def index(self, **kw):
@@ -19,3 +18,26 @@
 #         return http.request.render('filmotecajaime.object', {
 #             'object': obj
 #         })
+
+
+from odoo import http
+from odoo.http import Response
+import json
+
+
+class pelicula_controller(http.Controller):
+
+    @http.route("/api/peliculas", auth="public", method=["GET"], csrf=False)
+    def get_peliculas(self, **kw):
+        try:
+            peliculas = http.request.env["filmotecajaime.pelicula"].sudo().search_read([], ["id", "name", "color"])
+            res = json.dumps(peliculas, ensure_ascii=False).encode("utf-8")
+            return Response(
+                res, content_type="application/json;charset=utf-8", status=200
+            )
+        except Exception as e:
+            return Response(
+                json.dumps({"error": str(e)}),
+                content_type="application/json;charset=utf-8",
+                status=505,
+            )
